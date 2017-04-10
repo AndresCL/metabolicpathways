6959c6aa1359e494592e69064d2e795b5f7a533c
Andrés <aecontrerasl@gmail.com>
2017-04-10 13:23:04 -0300

    Removing extra modules

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,7 @@
 import { Routes, RouterModule } from '@angular/router';
 
-import { ExampleComponent } from './example/example.component';
-import { AboutComponent } from './about/about.component';
-
 const ROUTES: Routes = [
-    {
-        path: '',
-        component: ExampleComponent
-    },
-    {
-        path: 'about',
-        component: AboutComponent
-    }
+
 ];
 
 export const ROUTING = RouterModule.forRoot(ROUTES);
