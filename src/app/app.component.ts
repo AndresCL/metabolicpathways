529d9ddb919810add7ed65290e90dc3a461b7ee9
Andrés <aecontrerasl@gmail.com>
2017-04-12 13:38:52 -0300

    Stable version with some bugs to correct when more than one arrow path case happens

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -36,15 +36,12 @@ declare var Materialize: any;
             <div class="gen-container">
 
                 <!-- Toolbox -->
-                <load-pathway></load-pathway>
+                <load-pathway (jsonLoaded)="loadjson($event)"></load-pathway>
                 <color-scheme></color-scheme>
                  
                 <div id="mp_map">
                 </div>
-                <p>
-                    <a [routerLink]="['/']">Home</a> |
-                    <a [routerLink]="['/about']">About</a>
-                </p>
+                
                 <router-outlet></router-outlet>
             </div>
 
@@ -53,27 +50,7 @@ declare var Materialize: any;
 })
 export class AppComponent implements OnInit{
     public isTestable: boolean = true;
-
-    constructor(){
-        //called first time before the ngOnInit()
-        
-    }
-
-    ngOnInit(){
-
-        console.log("Init!");
-        
-        // Load a default map
-        var self = this;
-        d3.json('e_coli_core.Core metabolism.json', function(e: any, data: any) {
-            if (e) console.warn(e);
-            
-            console.log("loading json");
-            // ---------------------------------------
-            // First map: Just show the map
-            // ---------------------------------------
-
-            let options1 = {
+    public escher_options: any = {
                 // Just show the zoom buttons
                 menu: 'zoom',
                 // use the smooth pan and zoom option
@@ -86,55 +63,123 @@ export class AppComponent implements OnInit{
                 enable_tooltips: true
             };
 
-            // Building escher map
-            escher.Builder(data, null, null, d3.select('#mp_map'), options1);
-            console.log("Building escher");
+    constructor(){}
 
-            // Counting nodes
-            let node_types: Array<number> = [];
-            d3.selectAll("#nodes .node").each(function(d: any) {
+    ngOnInit(){
 
-                if(d.node_type != "midmarker" && d.node_type != "multimarker"){
-                    if(node_types[d.node_type] == undefined) node_types[d.node_type]=1; 
-                    else node_types[d.node_type]++;
-                }
+        // Load a default map
+        var self = this;
+        d3.json('e_coli_core.Core metabolism.json', function(e: any, data: any) {
+            if (e) console.warn(e);
 
-            });
-
-            // Showing statistics as toast
-            for (var property in node_types) {
-                if (node_types.hasOwnProperty(property)) {
-                    
-                    // Show statistics in toast
-                    Materialize.toast(property + ': ' + node_types[property], 6000);
-                }
-            }
-
-            // Get click on segment group
-            
-            d3.selectAll(".segment-group").on('click', function(d: any, i: any) {
-                console.log("Segment id" + d.segment_id);
-                console.log("From: " + d.from_node_id);
-                console.log("To: " + d.to_node_id);
-                console.log(d3.select("#n" + d.from_node_id).node());
-                console.log(d3.select("#n" + d.to_node_id).node());
-                //console.log(d3.select(this));
-
-                // Getting root/end nodes as array, as it could be more than one on each case
-                let rootNode = self.findRootNode(d.segment_id, d.from_node_id, [], []).join(" - ");
-                let endNode = self.findEndNode(d.segment_id, d.from_node_id, [], []).join(" - ");
-                
-                let html = '<strong>From Node:</strong> ' + rootNode + ' <strong>To:</strong> ' + endNode;
-
-                Materialize.toast(html, 10000);
-            });
+            // Build escher with data
+            self.escher_build(data);
             
         });
         
     }
 
+    escher_build(data: any){
+        
+        var self = this;
+
+        // Building escher map
+        escher.Builder(data, null, null, d3.select('#mp_map'), this.escher_options);
+        console.log("Building escher");
+
+        // Counting nodes
+        let node_types: Array<number> = [];
+        d3.selectAll("#nodes .node").each(function(d: any) {
+
+            if(node_types[d.node_type] == undefined) node_types[d.node_type]=1; 
+            else node_types[d.node_type]++;
+
+        });
+
+        // Showing statistics as toast
+        for (var property in node_types) {
+            if (node_types.hasOwnProperty(property)) {
+                
+                // Show statistics in toast
+                Materialize.toast(property + ': ' + node_types[property], 6000);
+            }
+        }
+
+        // Get click on segment group
+        d3.selectAll(".segment-group").on('click', function(d: any, i: any) {
+
+            // Getting root/end nodes as array, as it could be more than one on each case
+            let rootNode, endNode, from_node_id, to_node_id;
+
+            // If we are finding root node and we are already on it
+            if(d.from_node_coefficient < 0 && d.reversibility == false){
+                from_node_id = d.from_node_id; 
+            }
+            // If reversible lets find the other way
+            else if(d.reversibility == true){
+                from_node_id = d.to_node_id; 
+            }
+            // Otherwise we send to node id
+            else from_node_id = d.from_node_id;
+
+            rootNode = self.findRootNode(d.segment_id, from_node_id, [], []).join(" - ");
+
+            // If we are finding end node and we are already on it
+            if(d.to_node_coefficient > 0 && d.reversibility == false) {
+                to_node_id = d.to_node_id;
+            }
+            // If reversible lets find the other way
+            else if(d.reversibility == true){
+                to_node_id = d.from_node_id;
+            }
+            // Else
+            else to_node_id = d.to_node_id;
+
+            endNode = self.findEndNode(from_node_id, d.segment_id, to_node_id, [], []).join(" - ");
+
+            let html = 'From Node: ' + rootNode + ', To: ' + endNode;
+
+            Materialize.toast(html, 15000);
+        });
+    }
+
+    // On change event from template
+    loadjson(evt: any) {
+       
+        var self = this;
+
+        var files = evt.target.files; // FileList object
+
+        // Loop through the FileList and render image files as thumbnails.
+        for (var i = 0, f; f = files[i]; i++) {
+
+            let reader = new FileReader();
+
+            // Closure to capture the file information.
+            reader.onload = (function(theFile) {
+                
+                return function(evt: any) {
+
+                    // Load uploaded json in variable
+                    let json = JSON.parse(evt.target.result);
+
+                    // Build escher with data
+                    self.escher_build(json);
+
+                };
+            
+            })(f);
+
+            // Read in the json file as text.
+            reader.readAsText(f);
+        }
+    
+
+    } 
+
     // Finds end node for a segment
-    findEndNode(segment_id: number, to_node_id: string, visited_segments: Array<string> = [], endNodes: Array<string>): Array<string>{
+    findEndNode(from_node_id: number, segment_id: number, to_node_id: string, 
+                visited_segments: Array<string> = [], endNodes: Array<string>): Array<string>{
 
         var self = this;
 
@@ -151,12 +196,15 @@ export class AppComponent implements OnInit{
                     d3.select("#s" + d.connected_segments[i].segment_id).each(function(s: any){
                         
                         // Avoid to call same node again, as its listed on connected_segments
-                        if(!visited_segments.includes(s.segment_id) && s.segment_id != segment_id){
+                        if(!visited_segments.includes(s.segment_id)){
+                            
                             console.log("going to segment: " + s.segment_id);
                             visited_segments.push(s.segment_id);
 
                             // Call to iterate in possible other node segments
-                            endNodes = self.findEndNode(s.segment_id, s.to_node_id, visited_segments, endNodes);
+                            endNodes = self.findEndNode(from_node_id, s.segment_id, s.to_node_id, 
+                                                            visited_segments, endNodes);
+                        
                         }
                     });
                 }
@@ -164,8 +212,8 @@ export class AppComponent implements OnInit{
             // If type is metabolite its a starting node
             else { //if(d.node_type == "metabolite") {
 
-                if(!endNodes.includes(d.bigg_id + '(' + d.name + ')')){
-                    endNodes.push(d.bigg_id + '(' + d.name + ')');
+                if(!endNodes.includes(d.bigg_id) && d.node_id != from_node_id){
+                    endNodes.push(d.bigg_id);
                 }
             }
         });
@@ -187,7 +235,7 @@ export class AppComponent implements OnInit{
             if(d.node_type == "multimarker" || d.node_type == "midmarker"){
                 console.log("Visiting node: " + d.node_id);
 
-                // Explore it segments
+                // Explore it segments 
                 for(let i=0; i<d.connected_segments.length; i++){
                     
                     // Select segment
@@ -195,7 +243,7 @@ export class AppComponent implements OnInit{
                         
 
                         // Avoid to call same node again, as its listed on connected_segments
-                        if(!visited_segments.includes(s.segment_id) && s.segment_id != segment_id){
+                        if(!visited_segments.includes(s.segment_id)){
                             console.log("going to segment: " + s.segment_id);
                             visited_segments.push(s.segment_id);
 
@@ -208,8 +256,8 @@ export class AppComponent implements OnInit{
             // If type is metabolite its a starting node
             else { //if(d.node_type == "metabolite") {
 
-                if(!rootNodes.includes(d.bigg_id + '(' + d.name + ')')){
-                    rootNodes.push(d.bigg_id + '(' + d.name + ')');
+                if(!rootNodes.includes(d.bigg_id)){
+                    rootNodes.push(d.bigg_id);
                 }
             }
         });
--- src/app/load-pathway/loadpathway.component.ts
+++ src/app/load-pathway/loadpathway.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 // Declaring vars for libraries
 declare var escher: any;
@@ -11,75 +11,12 @@ declare var Materialize: any;
 })
 export class LoadpathwayComponent { 
     
-    // On change event from template
-    loadjsonfile(evt: any) {
-       
-        var files = evt.target.files; // FileList object
+    @Output() jsonLoaded = new EventEmitter();
 
-        // Loop through the FileList and render image files as thumbnails.
-        for (var i = 0, f; f = files[i]; i++) {
+    // Listen click event
+    loadjsonfile($event: any){
 
-            let reader = new FileReader();
-
-            // Closure to capture the file information.
-            reader.onload = (function(theFile) {
-                
-                return function(evt: any) {
-
-                    //console.log(evt);
-
-                    // Load uploaded json in variable
-                    let json = JSON.parse(evt.target.result);
-                        
-                    //console.log("loading json");
-                    // ---------------------------------------
-                    // First map: Just show the map
-                    // ---------------------------------------
-
-                    let options1 = {
-                        // Just show the zoom buttons
-                        menu: 'zoom',
-                        // use the smooth pan and zoom option
-                        use_3d_transform: true,
-                        // No editing in this map
-                        enable_editing: true,
-                        // No keyboard shortcuts 
-                        enable_keys: false,
-                        // No tooltips
-                        enable_tooltips: true
-                    };
-
-                    escher.Builder(json, null, null, d3.select('#mp_map'), options1);
-                    //console.log("Building escher");
-
-                    // Counting nodes
-                    let node_types: Array<number> = [];
-                    d3.selectAll("#nodes .node").each(function(d: any) {
-
-                        if(node_types[d.node_type] == undefined) node_types[d.node_type]=1; 
-                        else node_types[d.node_type]++;
-
-                    });
-
-                    // Showing statistics as toast
-                    for (var property in node_types) {
-                        if (node_types.hasOwnProperty(property)) {
-                            
-                            // do stuff
-                            Materialize.toast(property + ': ' + node_types[property], 6000);
-                        }
-                    }
-
-                    
-
-                };
-            
-            })(f);
-
-            // Read in the json file as text.
-            reader.readAsText(f);
-        }
-    
-
-    } 
+        // Emit event to parent
+        this.jsonLoaded.emit($event);
+    }
 }
